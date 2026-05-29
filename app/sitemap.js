32ea9a12b00f0ddb2d198470ee92cc620092d3be
Mzoxolo Beqezi <nmas3729@gmail.com>
2026-05-29 18:46:23 +0200

    Updated website files

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://access.nmas.co.za',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
